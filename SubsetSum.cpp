98daf9d21f8c67bb8561a8e04b8f4659db65e57a
mdshohed <mdshohed170@gmail.com>
2022-08-09 01:50:37 +0600

    subsetSum update

--- SubsetSum.cpp
+++ SubsetSum.cpp
@@ -1,8 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define all(x) begin(x),end(x)
-#define pii pair<ll,ll> 
 
 bool subsetSum(vector<int> a, int n, int sum) {
     vector<vector<bool>> dp(n+1, vector<bool>(sum+1));
@@ -18,23 +15,10 @@ bool subsetSum(vector<int> a, int n, int sum) {
             }
         }
     }
-    for(int i = 0; i<=n; i++) {
-        for(int j = 0; j<=sum; j++) {
-            cout << dp[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
     return dp[n][sum]; 
 }
 
 int main(){
-#ifndef LOCAL
-    ios_base::sync_with_stdio(false),cin.tie(nullptr);
-#else
-    freopen( "in.txt", "r", stdin);
-    //freopen( "out.txt","w", stdout);
-#endif
     std::vector<int> a = {1, 3, 4, 7, 10};
     int sum = 9;
     int n = a.size();
